622ee03e9fdd69b666878d71011c6187f3cfe7d2
満園大輔 <daigoren5@gmail.com>
2019-05-29 01:04:41 +0900

    ランク表示

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import { ButtonGroup,ListItem} from 'react-native-elements'; 
+import { ScrollView } from 'react-native-gesture-handler';
 
 const ALL_INDEX = 0;
 
 const GREAT = 'sentiment-very-satisfied';
 const GREAT_INDEX = 1;
+const GREAT_COLOR = 'red';
 
 const GOOD = 'sentiment-satisfied';
 const GOOD_INDEX = 2;
+const GOOD_COLOR = 'orange';
 
 const POOR = 'sentiment-dissatisfied';
 const POOR_INDEX = 3;
+const POOR_COLOR = 'blue';
 
 const allReviewsTmp =[
   {
     country:'USA',
     dateFrom:'Jan/15/2018',
+    dateTo:'Jan/25/2018',
     imageURIs: [
       require('../assets/add_image_placeholder.png'),
       require('../assets/add_image_placeholder.png'),
@@ -49,13 +54,6 @@ const allReviewsTmp =[
 
 ];
 
-let rankedReviews = [];
-
-for (let i = 0; i< allReviewsTmp.length; i++){
-  if(allReviewsTmp[i].rank === reviewRank) {
-    rankedReviews.push(allReviewsTmp[i])
-  }
-}
 
 
 
@@ -89,8 +87,55 @@ class HomeScreen extends React.Component {
         break;
 
     }
-    return (
 
+let rankedReviews = [];
+
+if(this.state.selectedIndex ===ALL_INDEX) {
+  rankedReviews = allReviewsTmp;
+
+
+}else{
+  for (let i = 0; i < allReviewsTmp.length; i++){
+    if(allReviewsTmp[i].rank === reviewRank) {
+      rankedReviews.push(allReviewsTmp[i]);
+    }
+  }
+}
+    return (
+    <ScrollView>
+      {rankedReviews.map((review,index) => {
+        let reviewColor;
+
+        switch (review.rank){
+          case GREAT:
+            reviewColor = GREAT_COLOR;
+          break;
+
+          case GOOD:
+            reviewColor = GOOD_COLOR;
+            break;
+
+          case POOR:
+            reviewColor = POOR_COLOR;
+            break;
+
+            default:
+              break;
+
+        }
+
+        return (
+          <ListItem 
+            key={index}
+            leftIcon={{name:review.rank, color: reviewColor}}
+            title={review.country}
+            subtitle={`${review.dateFrom} ~ ${review.dateTo}`}
+          />
+        );
+      })
+      
+      }
+    </ScrollView>
     );
   }
 
@@ -122,7 +167,7 @@ class HomeScreen extends React.Component {
   }
 }
 
-//こんちには
+
 
 
 export default HomeScreen;
