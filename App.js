c86015c504a6dd13bfcbc2c71507879b16cdadc9
満園大輔 <daigoren5@gmail.com>
2019-05-28 18:20:18 +0900

    first

---
+++ App.js
@@ -0,0 +1,173 @@
+import React from 'react';
+import { StyleSheet, View ,Platform, Image,StatusBar} from 'react-native';
+import { 
+  createAppContainer,　
+  createBottomTabNavigator,
+  createSwitchNavigator,
+  createStackNavigator
+} from 'react-navigation'; 
+
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import HomeScreen from './screens/HomeScreen'; 
+import DetailScreen from './screens/DetailScreen';
+import AddScreen from './screens/AddScreen'; 
+import ProfileScreen from './screens/ProfileScreen'; 
+import Setting1Screen from './screens/Setting1Screen'; 
+import Setting2Screen from './screens/Setting2Screen'; 
+
+
+export default class App extends React.Component {
+  render() {
+
+    const headerNavigationOptions ={
+      headerStyle: {
+        backgroundColor:'deepskyblue',
+        marginTop: (Platform.OS === 'android' ? 24 : 0)
+      },
+      headerTitleStyle:{color:'white'},
+      headerTintColor:'white',
+    
+    };
+
+    const HomeStack = createStackNavigator({ 
+      home: { 
+        screen: HomeScreen,
+        navigationOptions:{
+        ...headerNavigationOptions,
+        headerTitle: 'Treco',
+        headerBackTitle: 'Home'
+        }
+      },
+
+      detail: {
+        screen: DetailScreen,
+        navigationOptions:{
+          ...headerNavigationOptions,
+          headerTitle :'Detail',
+        }
+      }
+    });
+    
+    HomeStack.navigationOptions = ({ navigation }) => {
+      return {
+        tabBarVisible: (navigation.state.index === 0)
+      };
+    };
+
+
+    const AddStack = createStackNavigator({ 
+      add: { 
+        screen: AddScreen,
+        navigationOptions:{
+          header:null
+        }
+      }
+    });
+    
+    AddStack.navigationOptions = ({ navigation }) => {
+      return {
+        tabBarVisible: (navigation.state.index === -1)
+      };
+    };
+
+    const ProfileStack = createStackNavigator({ 
+      profile: { 
+        screen: ProfileScreen,
+        navigationOptions:{
+          ...headerNavigationOptions,
+          headerTitle:'Treco',
+          headerBackTitle:'Profile'
+        } 
+      },
+      setting1: { 
+        screen: Setting1Screen,
+        navigationOptions:{
+          ...headerNavigationOptions,
+          headerTitle:'Setting 1',
+        } 
+      },
+      setting2: {
+        screen: Setting2Screen,
+        navigationOptions:{
+          ...headerNavigationOptions,
+          headerTitle:'Setting 2',
+        }
+      }
+    });
+
+    ProfileStack.navigationOptions = ({ navigation }) => {
+      return {
+        tabBarVisible: (navigation.state.index ===0 )
+      };
+    };
+    
+    const MainTab = createBottomTabNavigator({
+      homeStack: { 
+        screen: HomeStack,
+        navigationOptions:{
+          tabBarIcon:({tintColor}) => (
+            <Image 
+              style={{height:25, width:25, tintColor}}
+              source={require('./assets/home.png')}
+            />
+          ),
+          title: 'Home' 
+        }
+      }, 
+      addStack: { 
+        screen: AddStack,
+        navigationOptions:{
+          tabBarIcon:() => (
+          <Image
+            style={{height:60, width:60, tintColor: 'deepskyblue'}}
+            source={require('./assets/add.png')}
+          />
+          ),
+        title: '',
+        },
+      },
+  
+      profileStack: { 
+        screen: ProfileStack,
+        navigationOptions:{
+        tabBarIcon:({ tintColor }) => (
+          <Image
+            style={{height:25, width:25, tintColor:tintColor}}
+            source={require('./assets/profile.png')}
+          />
+        ),
+        title: 'Profile'
+        }
+     } 
+    },
+    {
+      swipeEnabled: false,
+    });
+    
+    const NavigatorTab = createAppContainer(
+      createSwitchNavigator({
+        welcome: { screen: WelcomeScreen },
+        main: {screen: MainTab}
+      })
+    );
+
+  
+
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-contest" />
+        <NavigatorTab />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    justifyContent: 'center',
+  },
+});;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { ButtonGroup,ListItem} from 'react-native-elements'; 
+
+const ALL_INDEX = 0;
+
+const GREAT = 'sentiment-very-satisfied';
+const GREAT_INDEX = 1;
+
+const GOOD = 'sentiment-satisfied';
+const GOOD_INDEX = 2;
+
+const POOR = 'sentiment-dissatisfied';
+const POOR_INDEX = 3;
+
+const allReviewsTmp =[
+  {
+    country:'USA',
+    dateFrom:'Jan/15/2018',
+    imageURIs: [
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+    ],
+    rank: GREAT,
+  },
+  {
+    country: 'USA',
+    dateFrom: 'Feb/15/2018',
+    dateTo: 'Feb/25/2018',
+    imageURIs: [
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+    ],
+    rank: GOOD,
+  },
+  {
+    country: 'USA',
+    dateFrom: 'Mar/15/2018',
+    dateTo: 'Mar/25/2018',
+    imageURIs: [
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+      require('../assets/add_image_placeholder.png'),
+    ],
+    rank: POOR,
+  },
+
+];
+
+let rankedReviews = [];
+
+for (let i = 0; i< allReviewsTmp.length; i++){
+  if(allReviewsTmp[i].rank === reviewRank) {
+    rankedReviews.push(allReviewsTmp[i])
+  }
+}
+
+
+
+class HomeScreen extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      selectedIndex:0,
+    }
+  }
+
+  renderReviews() {
+    let reviewRank;
+
+    switch (this.state.selectedIndex){
+
+      case GREAT_INDEX:
+        reviewRank = GREAT;
+        break;
+
+      case GOOD_INDEX:
+        reviewRank = GOOD;
+        break;
+
+      case POOR_INDEX:
+        reviewRank = POOR;
+        break;
+
+      default:
+        break;
+
+    }
+    return (
+
+    );
+  }
+
+  onButtonGroupPress = (selectedIndex) => {
+    this.setState({
+      selectedIndex:selectedIndex
+    });
+  }
+
+
+  render() {
+    const buttonList =[
+      'All',
+      'Great (0)',
+      'Good (0)',
+      'Poor (0)'
+    ];
+
+    return (
+      <View style={{ flex: 1}}>
+        
+      <ButtonGroup
+        buttons = {buttonList}
+        selectedIndex={this.state.selectedIndex}
+        onPress={this.onButtonGroupPress}
+      /> 
+      </View>
+    );
+  }
+}
+
+
+export default HomeScreen;
