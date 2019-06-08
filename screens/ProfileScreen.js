90233d8972b5491eb3a1e8812fdeeb0e7389c6d5
満園大輔 <daigoren5@gmail.com>
2019-06-09 05:22:29 +0900

    create ResetButton

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,20 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage, Alert } from 'react-native';
 import {Button} from 'react-native-elements';
+import {connect} from 'react-redux';
+
+import * as actions from '../actions';
 
 class ProfileScreen extends React.Component {
+
+  onResetButtonPress = async(key) => {
+    await AsyncStorage.removeItem(key);
+
+    if(key === 'allReviews'){
+      this.props.fetchAllReviews();
+    }
+
+    Alert.alert(
+      'Reset',
+      `'${key}' in AsyncStorage has been removed.`,
+      [
+        {text:'OK'},
+      ],
+      {cancelable:false}
+    );
+  }
  render() {
    return (
      <View style={{ flex: 1, justifyContent: 'center' }}>
-       <Text>This is ProfileScreen</Text>
+       <View style={{padding:20}}>
        <Button
          title="Go to Setting1Screen"
          onPress={() => this.props.navigation.navigate('setting1')}
        />
+       </View>
+
+       <View style={{padding:20}}>
+         <Button
+           title="Reset welcome page"
+           buttonStyle={{backgroundColor:'red'}}
+           onPress={() => this.onResetButtonPress('isInitialized')}
+           />
+        </View>
+
+        <View style={{padding:20}}>
+          <Button
+            title="Reset all review data"
+            buttonStyle={{backgroundColor:'red'}}
+            onPress={() => this.onResetButtonPress('allReviews')}
+            />
+        </View>
+
      </View>
    );
  }
 }
 
+const mapStateToProps = (state) => {
+  return{
+    allReviews:state.review.allReviews,
+  };
+};
 
-export default ProfileScreen;
+
+export default connect(mapStateToProps, actions)(ProfileScreen);
